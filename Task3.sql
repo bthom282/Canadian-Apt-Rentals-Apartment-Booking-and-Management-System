33481c53e3a2a74ae3d34b205c1cc68d21d3da31
kchea186 <kchea186@mtroyal.ca>
2024-11-29 19:21:19 -0700

    Q: 4 & 5 completed

--- Task3.sql
+++ Task3.sql
@@ -8,7 +8,13 @@
 
 
 /* 4.) */
-
+SELECT b.BuildingID, SUM((TO_DAYS(bo.EndTime) - TO_DAYS(bo.StartTime)) * 100) AS TotalRevenue
+FROM Building AS b
+JOIN Apartment AS a ON b.BuildingID = a.BuildingID
+JOIN Booking AS bo ON a.ApartmentID = bo.ApartmentID
+WHERE bo.Status = 'Confirmed'
+OR bo.Status = 'Completed'
+GROUP BY b.BuildingID;
 
 /* 5.) */
 SELECT g.Name AS Name, COUNT(b.GuestID) AS Bookings
